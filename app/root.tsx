b8dc2c341308a7238bdb790844df97023ec1aea8
prithu12 <prithur045@gmail.com>
2025-08-31 23:25:42 +0530

    Authetication

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -24,6 +25,10 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const {init}=usePuterStore();
+  useEffect(()=>{
+    init();
+  },[init]);
   return (
     <html lang="en">
       <head>
@@ -33,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <script src="https://js.puter.com/v2/"></script>
         {children}
         <ScrollRestoration />
         <Scripts />
